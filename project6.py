a70d97614a762143a3a0fa4d5bd49c83022e2b84
dayeon <dayeon20060527h@gmail.com>
2026-03-11 06:48:55 +0900

    project6

--- project6.py
+++ project6.py
@@ -1,29 +1,15 @@
-INF = int(1e9)
-N, M = map(int, input().split())
+# 사용자로부터 n 입력받기
+n = int(input())
 
-# 2차원 배열 초기화
-graph = [[INF] * (N + 1) for _ in range(N + 1)]
-for i in range(1, N + 1):
-    graph[i][i] = 0
+# 메모이제이션을 위한 리스트 초기화 (n이 90까지 가능하므로 넉넉하게 91개 생성)
+memo = [0] * 91
 
-# 도로 정보 입력
-for _ in range(M):
-    a, b = map(int, input().split())
-    graph[a][b] = 1
-    graph[b][a] = 1
+# 기본값 설정
+memo[1] = 1
 
-X, K = map(int, input().split())
+# 2부터 n까지 차례대로 계산하며 리스트에 저장
+for i in range(2, n + 1):
+    memo[i] = memo[i-1] + memo[i-2]
 
-# 플로이드-워셜 핵심 로직
-for k in range(1, N + 1):
-    for i in range(1, N + 1):
-        for j in range(1, N + 1):
-            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
-
-# 최종 결과 계산
-distance = graph[1][X] + graph[X][K]
-
-if distance >= INF:
-    print("-1")
-else:
-    print(distance)
+# 결과 출력
+print(memo[n])
